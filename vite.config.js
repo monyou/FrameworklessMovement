51f243700797283371d4d3ebe0aa41dc8e9d3d7d
Simeon Mechkov <montishak@gmail.com>
2025-07-07 19:17:30 +0300

    Vercel hosting

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,8 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-    root: "src",
     plugins: [tailwindcss()],
     build: {
         outDir: "dist",
-        emptyOutDir: true,
     },
 });
